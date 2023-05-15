0a27ad52f9eef664d4e881204e1ff1e7efbcfa70
SekoViper <damehlivingstone1@gmail.com>
2023-05-15 12:27:38 +0000

    Add footer section.

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './footer.css';
+import GithubIcon from '../../assets/img/github_btn_icon.svg';
+import TwitterIcon from '../../assets/img/twiter_icon.svg';
+import LinkedinIcon from '../../assets/img/Linkedin_icon.svg';
+import MediumIcon from '../../assets/img/medium_icon.svg';
+
+const Footer = () => (
+  <div>
+    <footer>
+      <div>
+        <h3>SekoViper</h3>
+      </div>
+      <div>
+        <h4>Quick Links</h4>
+        <a href="/">Home</a>
+        <a href="/projects">Projects</a>
+      </div>
+      <div>
+        <h4>Contact</h4>
+        <a href="/contact">Contact</a>
+      </div>
+      <div>
+        <h4>Connect with me</h4>
+        <div className="social-icons">
+          <a href="https://github.com/SekoViper" target="_blank" rel="noreferrer"><img src={GithubIcon} alt="Github" /></a>
+          <a href="https://twitter.com/sekoviper" target="_blank" rel="noreferrer"><img src={TwitterIcon} alt="Twiter" /></a>
+          <a href="https://linkedin.com/in/livingstone-dameh-b755a5151" target="_blank" rel="noreferrer"><img src={LinkedinIcon} alt="Github" /></a>
+          <a href="https://github.com/SekoViper" target="_blank" rel="noreferrer"><img src={MediumIcon} alt="Github" /></a>
+        </div>
+      </div>
+    </footer>
+  </div>
+);
+
+export default Footer;

8fb9eeec981d9d62ac941c391cc9a488bc8776d8
SekoViper <damehlivingstone1@gmail.com>
2023-05-15 10:38:32 +0000

    Modify

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './home.css';
+import Project from '../projects/Project';
 import profilePic from '../../assets/img/Seko _Dameh_Livingstone_about_img.png';
 import GithubIcon from '../../assets/img/github_btn_icon.svg';
 import TwitterIcon from '../../assets/img/twiter_icon.svg';
@@ -8,49 +9,52 @@ import MediumIcon from '../../assets/img/medium_icon.svg';
 
 const Home = () => (
   <>
-    <main className="home-container">
-      <div>
-        <h1>
-          Hi I&apos;m &nbsp;
-          <span className="firstname">Livingstone</span>
-        </h1>
-        <h2>Full-stack Developer</h2>
-      </div>
-      <p>
-        A highly skilled and passionate full-stack developer
-        hailing from the beautiful country of Ghana. With years of
-        experience under his belt, Livingstone has a proven track
-        record of delivering high-quality web and mobile
-        applications that meet and exceed client expectations.
-      </p>
-      <div className="social-icons">
-        <img src={GithubIcon} alt="Github" />
-        <img src={TwitterIcon} alt="Twiter" />
-        <img src={LinkedinIcon} alt="LinkedIn" />
-        <img src={MediumIcon} alt="Medium" />
-      </div>
-    </main>
+    <main>
+      <section className="home-container">
+        <div>
+          <h1>
+            Hi I&apos;m &nbsp;
+            <span className="firstname">Livingstone</span>
+          </h1>
+          <h2>Full-stack Developer</h2>
+        </div>
+        <p>
+          A highly skilled and passionate full-stack developer
+          hailing from the beautiful country of Ghana. With years of
+          experience under his belt, Livingstone has a proven track
+          record of delivering high-quality web and mobile
+          applications that meet and exceed client expectations.
+        </p>
+        <div className="social-icons">
+          <img src={GithubIcon} alt="Github" />
+          <img src={TwitterIcon} alt="Twiter" />
+          <img src={LinkedinIcon} alt="LinkedIn" />
+          <img src={MediumIcon} alt="Medium" />
+        </div>
+      </section>
 
-    <section className="about-section">
-      <div className="img">
-        <img src={profilePic} alt="Profile" />
-      </div>
-      <div className="content">
-        <h2>About Me</h2>
-        <p>
-          Meet Livingstone, a highly skilled and passionate full-stack developer
-          hailing from the beautiful country of Ghana. With years of experience under
-          his belt, Livingstone has a proven track record of delivering high-quality web
-          and mobile applications that meet and exceed client expectations.
-        </p>
-        <p>
-          Livingstone’s passion for programming started at a young age and has only
-          grown stronger over the years.
-        </p>
-        <p>Here are some of the technologies I use to build dynamic web apps:</p>
-        <button className="btn-dorminant" type="button">get to know more</button>
-      </div>
-    </section>
+      <section className="about-section">
+        <div className="img">
+          <img src={profilePic} alt="Profile" />
+        </div>
+        <div className="content">
+          <h2>About Me</h2>
+          <p>
+            Meet Livingstone, a highly skilled and passionate full-stack developer
+            hailing from the beautiful country of Ghana. With years of experience under
+            his belt, Livingstone has a proven track record of delivering high-quality web
+            and mobile applications that meet and exceed client expectations.
+          </p>
+          <p>
+            Livingstone’s passion for programming started at a young age and has only
+            grown stronger over the years.
+          </p>
+          <p>Here are some of the technologies I use to build dynamic web apps:</p>
+          <button className="btn-dorminant" type="button">get to know more</button>
+        </div>
+      </section>
+      <Project />
+    </main>
   </>
 );
 
--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import './project.css';
+import TvShows from '../../assets/img/tv_shows.png';
 
 const Project = () => (
-  <div>Project</div>
+  <section className="projects-section">
+    <h2 className="projects-tiltle">Projects</h2>
+    <div className="projects">
+      <div className="project">
+        <div className="project-img">
+          <img src={TvShows} alt="Tv Shows" />
+        </div>
+        <div className="project-content">
+          <h3>TV Shows</h3>
+          <p>
+            A web application that allows users to search for their favorite TV
+            shows and get details about them.
+            It also allows users to create an account and save their favorite TV shows.
+            This project was built using React, Redux, and the TV Maze API.
+          </p>
+          <div className="project-built">
+            <span>React</span>
+            <span>Redux</span>
+            <span>API</span>
+          </div>
+          <div className="project-links">
+            <a className="project-link" href="#!">View Live</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="projects">
+      <div className="project">
+        <div className="project-img">
+          <img src={TvShows} alt="Tv Shows" />
+        </div>
+        <div className="project-content">
+          <h3>TV Shows</h3>
+          <p>
+            A web application that allows users to search for their favorite TV
+            shows and get details about them.
+            It also allows users to create an account and save their favorite TV shows.
+            This project was built using React, Redux, and the TV Maze API.
+          </p>
+          <div className="project-built">
+            <span>React</span>
+            <span>Redux</span>
+            <span>API</span>
+          </div>
+          <div className="project-links">
+            <a className="project-link" href="#!">View Live</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 );
 
 export default Project;

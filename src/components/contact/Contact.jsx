7e2e331888da352e7d69402500fe441ceb9ff2ed
SekoViper <damehlivingstone1@gmail.com>
2023-05-15 11:12:42 +0000

    Modified.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import './contact.css';
 
 const Contact = () => (
-  <div>Contact</div>
+  <div className="container">
+    <h1>Get intouch to me</h1>
+    <div>
+      <form action="">
+        <input type="text" required name="name" id="name" placeholder="Name" />
+        <input type="email" required name="email" id="email" placeholder="Email" />
+        <textarea name="message" required id="message" cols="30" rows="10" placeholder="Message" />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  </div>
 );
 
 export default Contact;

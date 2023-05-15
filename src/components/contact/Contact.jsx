ed1985d1241848f840ac38254de94410ee81ca87
SekoViper <damehlivingstone1@gmail.com>
2023-05-15 12:29:07 +0000

    Modify contact page.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import './contact.css';
+import Footer from '../footer/Footer';
 
 const Contact = () => (
-  <div className="container">
-    <h1>Get intouch to me</h1>
-    <div>
-      <form action="">
-        <input type="text" required name="name" id="name" placeholder="Name" />
-        <input type="email" required name="email" id="email" placeholder="Email" />
-        <textarea name="message" required id="message" cols="30" rows="10" placeholder="Message" />
-        <button type="submit">Send</button>
-      </form>
+  <>
+    <div className="container">
+      <h1>Get intouch to me</h1>
+      <div>
+        <form action="">
+          <input type="text" required name="name" id="name" placeholder="Name" />
+          <input type="email" required name="email" id="email" placeholder="Email" />
+          <textarea name="message" required id="message" cols="30" rows="10" placeholder="Message" />
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
-  </div>
+    <Footer />
+  </>
 );
 
 export default Contact;

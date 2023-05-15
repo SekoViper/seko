006ad79e46379e346e32ce014c9fd18588d3048e
SekoViper <damehlivingstone1@gmail.com>
2023-05-15 12:29:26 +0000

    Modify.

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,60 +1,64 @@
 import React from 'react';
 import './project.css';
 import TvShows from '../../assets/img/tv_shows.png';
+import Footer from '../footer/Footer';
 
 const Project = () => (
-  <section className="projects-section">
-    <h2 className="projects-tiltle">Projects</h2>
-    <div className="projects">
-      <div className="project">
-        <div className="project-img">
-          <img src={TvShows} alt="Tv Shows" />
-        </div>
-        <div className="project-content">
-          <h3>TV Shows</h3>
-          <p>
-            A web application that allows users to search for their favorite TV
-            shows and get details about them.
-            It also allows users to create an account and save their favorite TV shows.
-            This project was built using React, Redux, and the TV Maze API.
-          </p>
-          <div className="project-built">
-            <span>React</span>
-            <span>Redux</span>
-            <span>API</span>
+  <>
+    <section className="projects-section">
+      <h2 className="projects-tiltle">Projects</h2>
+      <div className="projects">
+        <div className="project">
+          <div className="project-img">
+            <img src={TvShows} alt="Tv Shows" />
           </div>
-          <div className="project-links">
-            <a className="project-link" href="#!">View Live</a>
+          <div className="project-content">
+            <h3>TV Shows</h3>
+            <p>
+              A web application that allows users to search for their favorite TV
+              shows and get details about them.
+              It also allows users to create an account and save their favorite TV shows.
+              This project was built using React, Redux, and the TV Maze API.
+            </p>
+            <div className="project-built">
+              <span>React</span>
+              <span>Redux</span>
+              <span>API</span>
+            </div>
+            <div className="project-links">
+              <a className="project-link" href="#!">View Live</a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div className="projects">
-      <div className="project">
-        <div className="project-img">
-          <img src={TvShows} alt="Tv Shows" />
-        </div>
-        <div className="project-content">
-          <h3>TV Shows</h3>
-          <p>
-            A web application that allows users to search for their favorite TV
-            shows and get details about them.
-            It also allows users to create an account and save their favorite TV shows.
-            This project was built using React, Redux, and the TV Maze API.
-          </p>
-          <div className="project-built">
-            <span>React</span>
-            <span>Redux</span>
-            <span>API</span>
+      <div className="projects">
+        <div className="project">
+          <div className="project-img">
+            <img src={TvShows} alt="Tv Shows" />
           </div>
-          <div className="project-links">
-            <a className="project-link" href="#!">View Live</a>
+          <div className="project-content">
+            <h3>TV Shows</h3>
+            <p>
+              A web application that allows users to search for their favorite TV
+              shows and get details about them.
+              It also allows users to create an account and save their favorite TV shows.
+              This project was built using React, Redux, and the TV Maze API.
+            </p>
+            <div className="project-built">
+              <span>React</span>
+              <span>Redux</span>
+              <span>API</span>
+            </div>
+            <div className="project-links">
+              <a className="project-link" href="#!">View Live</a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+    <Footer />
+  </>
 );
 
 export default Project;

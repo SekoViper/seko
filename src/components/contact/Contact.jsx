c70d6cdf3140c5f14a0655e61a6f933dcabcff57
SekoViper <damehlivingstone1@gmail.com>
2023-05-24 09:18:21 +0000

    Add call to action.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,6 +5,8 @@ const Contact = () => (
   <>
     <div className="container">
       <h1>Get In Touch With Me</h1>
+      <p>I'm always interested in hearing about new projects, 
+        so if you'd like to chat please get in touch.</p>
       <div>
         <form
           id="fs-frm"

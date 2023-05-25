341d0ed955d0e0bdcf9a2d5f447ee27193bc6427
SekoViper <damehlivingstone1@gmail.com>
2023-05-25 16:15:36 +0000

    Add call to action to form.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,8 +5,10 @@ const Contact = () => (
   <>
     <div className="container">
       <h1>Get In Touch With Me</h1>
-      <p>I'm always interested in hearing about new projects, 
-        so if you'd like to chat please get in touch.</p>
+      <p>
+        I&apos;m always interested in hearing about new projects,
+        so if you&apos;d like to chat please get in touch.
+      </p>
       <div>
         <form
           id="fs-frm"

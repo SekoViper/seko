a0083923a4a712aa5229a9a3633752535d5c1e94
SekoViper <damehlivingstone1@gmail.com>
2023-05-28 22:16:14 +0000

    Add Project API file.

---
+++ src/API.js
@@ -0,0 +1,37 @@
+const projects = [
+    {
+      title: 'E-commerce App',
+      image: require('../../assets/img/shop.jpg').default,
+      description: 'The app aims to provide customers with a seamless and convenient shopping experience...',
+      technologies: ['React', 'Redux', 'TailwindCSS'],
+      liveLink: 'https://shop-comer.netlify.app/',
+      sourceLink: 'https://github.com/SekoViper/ecommerce',
+    },
+    {
+      title: 'TV Shows',
+      image: require('../../assets/img/tv_shows.png').default,
+      description: 'A web application that allows users to search for their favorite TV shows and get details about them...',
+      technologies: ['React', 'Redux', 'API'],
+      liveLink: 'https://sekoviper.github.io/Tv-Shows/dist/',
+      sourceLink: 'https://github.com/SekoViper/Tv-Shows',
+    },
+    {
+      title: 'Xpens App',
+      image: require('../../assets/img/budget.jpg').default,
+      description: 'This is an app that allows users to track their budget by keeping track of their transactions...',
+      technologies: ['Ruby', 'Rails', 'PostgreSQL'],
+      liveLink: 'https://bud-ppvp.onrender.com/',
+      sourceLink: 'https://github.com/SekoViper/bud',
+    },
+    {
+      title: 'NGC Developers Congress',
+      image: require('../../assets/img/NGC_Congress.jpg').default,
+      description: 'The NGC Developers event is a two-day conference designed for developers who want to learn...',
+      technologies: ['Javascript', 'HTML', 'CSS'],
+      liveLink: 'https://sekoviper.github.io/WorldDevCongress/',
+      sourceLink: 'https://github.com/SekoViper/WorldDevCongress',
+    },
+  ];
+  
+  export default projects;
+  
